e75a2102c9c7c5596ee59659c18c3baba8b98d0e
patrick piemonte <piemonte@users.noreply.github.com>
2020-06-18 22:57:55 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "Burst"
+    name: "Burst",
+    platforms: [
+      .iOS(.v10)
+    ],
+    products: [
+      .library(name: "Burst", targets: ["Burst"])
+    ],
+    targets: [
+      .target(
+          name: "Burst",
+          path: "Sources"
+      )
+    ],
+    swiftLanguageVersions: [.v5]
 )
